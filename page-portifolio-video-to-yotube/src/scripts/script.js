7b762282e9c3fa17a56fe567ce9f9085c8b853a9
IsaqueTADS <isaqueofc18@gmail.com>
2025-02-13 23:35:24 -0300

    site pro video finalizado

---
+++ page-portifolio-video-to-yotube/src/scripts/script.js
@@ -0,0 +1,18 @@
+const ButtonMenuHambu = document.querySelector(".bota-menu-hambu");
+const closeMenu = document.querySelector(".close-menu");
+const menuHamburguer = document.querySelector(".menu-hamburguer");
+
+console.log(ButtonMenuHambu);
+
+function ativarMenuHamburguer() {
+  menuHamburguer.classList.toggle("ativar");
+  closeMenu.classList.toggle("ativar");
+}
+function desativarMenuHamburguer() {
+  menuHamburguer.classList.remove("ativar");
+  closeMenu.classList.remove("ativar");
+}
+
+closeMenu.addEventListener("click", desativarMenuHamburguer);
+
+ButtonMenuHambu.addEventListener("click", ativarMenuHamburguer);
